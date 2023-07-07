6d9d5e2438fdf8fcf7aa20b6d89a92ac9df3446e
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-07-07 17:08:56 +0600

    Fixed Next Auth Session

---
+++ prompt-sharing-app/app/create-prompt/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreatePrompt = () => {
+  return (
+    <div>Create Promot</div>
+  )
+}
+
+export default CreatePrompt
--- prompt-sharing-app/components/Nav.jsx
+++ prompt-sharing-app/components/Nav.jsx
@@ -36,7 +36,7 @@ const Nav = () => {
                         </Link>
                         <button type='button' onClick={signOut} className='outline_btn'>Sign Out</button>
                         <Link href="/profile">
-                            <Image src="/assets/images/logo.svg"
+                            <Image src={session?.user.image}
                                 width={37}
                                 height={37}
                                 className='rounded-full'
@@ -64,7 +64,7 @@ const Nav = () => {
             <div className="sm:hidden flex relative">
                 {session?.user ? (
                     <div className='flex'>
-                        <Image src="/assets/images/logo.svg"
+                        <Image src={session?.user.image}
                             width={37}
                             height={37}
                             className='rounded-full'
@@ -102,7 +102,8 @@ const Nav = () => {
                                 Sign in
                             </button>
                         ))}
-                </>}
+                </>
+                }
             </div>
         </nav>
     )

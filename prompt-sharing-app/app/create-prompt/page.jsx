fcee89d8201975a303016855cef4cc7c15397498
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-07-12 16:19:13 +0600

    Create Post Form UI

--- prompt-sharing-app/app/create-prompt/page.jsx
+++ prompt-sharing-app/app/create-prompt/page.jsx
@@ -1,8 +1,29 @@
+'use client';
 import React from 'react'
-
+import { useState } from 'react'
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import Form from '@components/Form';
 const CreatePrompt = () => {
+    const [submitting, setSubmitting] = useState(false);
+    const [post, setPost] = useState({
+        prompt:'',
+        tag:''
+    });
+    const createPrompt = async(e) => {
+
+    }
+
   return (
-    <div>Create Promot</div>
+    <Form
+    type="Create"
+    post={post}
+    setPost={setPost}
+    submitting={submitting}
+    handleSubmit={CreatePrompt}
+    >
+        
+    </Form>
   )
 }
 

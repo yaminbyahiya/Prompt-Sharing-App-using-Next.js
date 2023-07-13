1829f6e29dd3faab8d337b1a67e324c6227849e8
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-07-13 19:48:47 +0600

    GET Request using useEffect

--- prompt-sharing-app/components/Feed.jsx
+++ prompt-sharing-app/components/Feed.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
-
-const Feed = () => {
+'use client';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import PromptCard from './PromptCard';
+const PromptCardList = ({data, handleTagClick}) => {
   return (
-    <div>Feed</div>
+    <div className='mt-16 prompt_layout'>
+      {
+        data.map((post)=>(
+          <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}></PromptCard>
+        ))
+      }
+    </div>
+  )
+}
+const Feed = () => {
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value);
+  }
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt');
+      const data = await response.json();
+      setPosts(data);
+    }
+  }, []);
+
+  console.log("posts", posts);
+  
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-container">
+        <input type="text" placeholder='Search for a tag or a username' value={searchText} onChange={handleSearchChange} className="search_input peer" />
+      </form>
+      <PromptCardList
+      data={posts}
+      handleTagClick={()=>{}}>
+      </PromptCardList>
+    </section>
   )
 }
 

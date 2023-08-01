4d5a2d43c8e77b0782a8948dd6a6057d84226750
yamin-b-yahiya <yaminbyahiya@gmail.com>
2023-08-02 01:02:35 +0600

    Edit & Delete UI on Profile using useSession & usePathname

--- prompt-sharing-app/components/PromptCard.jsx
+++ prompt-sharing-app/components/PromptCard.jsx
@@ -39,6 +39,16 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
       </div>
       <p className='my-4 font-satoshi text-sm text-gray-700'>{post.prompt}</p>
       <p className='font-inter text-sm blue_gradient cursor-pointer' onClick={()=> handleTagClick && handleTagClick(post.tag)}>{post.tag}</p>
+      {session?.user._id === post.creator.id && pathName === '/profile' && (
+        <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+          <p className="font-inter text-sm green_gradient cursor-pointer" onClick={handleEdit}>
+            Edit
+          </p>
+          <p className="font-inter text-sm orange_gradient cursor-pointer" onClick={handleEdit}>
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   )
 }

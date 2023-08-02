3f6ae90ef05fc9ee7c62fbd85ce6c6102f2b54c3
yamin-b-yahiya <yaminbyahiya@gmail.com>
2023-08-03 02:37:41 +0600

    GET & POST API Endpoints

---
+++ prompt-sharing-app/app/api/prompt/[id]/route.js
@@ -0,0 +1,41 @@
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const GET = async (request, {params}) => {
+    try {
+        await connectToDB();        
+        const prompts = await Prompt.find(params.id).populate('creator');
+        console.log("Prompts", prompts);
+        if(!prompts){
+            return new Response("Prompt not found!", {status:404});
+        }
+        return new Response(JSON.stringify(prompts),{
+            status:200
+        })
+    } catch (error) {
+        return new Response("Failed to fetch all prompts",{
+            status:500
+        })
+    }
+}
+
+export const PATCH = async (request, {params}) => {
+    const {prompt, tag} = await request.JSON();
+    try {
+        await connectToDB();
+        const existingPrompts = await Prompt.findById(params.id);
+        if(!existingPrompts){
+            return new Response("Prompt not found!", {status:404});
+        }
+        existingPrompts.prompt = prompt;
+        existingPrompts.tag = tag;
+        await existingPrompts.save();
+        return new Response(JSON.stringify(existingPrompts),{
+            status:200
+        })
+    } catch (error) {
+        return new Response("Failed to fetch all prompts",{
+            status:500
+        })
+    }
+}

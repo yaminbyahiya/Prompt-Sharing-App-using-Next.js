347c3330dd25d8a2e0858611a5e541ded2f151c0
yaminbyahiya <yaminbyahiya@gmail.com>
2023-08-28 13:04:15 +0600

    UpdatePrompt PATCH using useState and useEffect

--- prompt-sharing-app/app/profile/page.jsx
+++ prompt-sharing-app/app/profile/page.jsx
@@ -2,9 +2,10 @@
 
 import { useState, useEffect } from 'react';
 import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import Profile from '@components/Profile';
 const MyProfile = () => {
+    const router = useRouter();
     const { data: session } = useSession();
     const [posts, setPosts] = useState([]);
     const fetchPosts = async () => {
@@ -15,8 +16,8 @@ const MyProfile = () => {
     useEffect(() => {
         if (session?.user.id) { fetchPosts(); }
     }, []);
-    const handleEdit = () => {
-
+    const handleEdit = (post) => {
+        router.push(`/update-prompt?id=${post._id}`);
     }
 
     const handleDelete = async () => {
